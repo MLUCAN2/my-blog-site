9c807a58f65a51acb61241e36ae7b26382dc256e
MLUCAN2 <162901593+MLUCAN2@users.noreply.github.com>
2024-05-07 16:51:01 -0500

    added/updated folder structure and server JS

---
+++ server.js
@@ -0,0 +1,42 @@
+// Setup our modules
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers');
+const sequelize = require('./config/connection');
+const path = require('path');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Setup our handlebars
+const hbs = exphbs.create({});
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+
+// Setup our session for security, user auth, access
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+
+app.use(session(sess));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Server is alive!'));
+});
